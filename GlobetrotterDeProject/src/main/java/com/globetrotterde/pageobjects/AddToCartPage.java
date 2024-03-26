831a0d60368fb00b2b8b8a0112e4a4c09541a163
salo <salo@Sayrina-Fenix>
2024-03-26 20:18:09 +0530

    Updated Product Size to be selected

--- GlobetrotterDeProject/src/main/java/com/globetrotterde/pageobjects/AddToCartPage.java
+++ GlobetrotterDeProject/src/main/java/com/globetrotterde/pageobjects/AddToCartPage.java
@@ -79,7 +79,7 @@ public class AddToCartPage extends BaseClass {
 	
 	public void clickOnSize() throws Throwable {
 		
-		action.click(getDriver(), xlSize);
+		action.click(getDriver(), sSize);
 		Thread.sleep(30);
 	}
 	
